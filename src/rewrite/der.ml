aaa3ff358f6940bf4fb8bd0081a309115a2b1dcd
Marc Pouzet <Marc.Pouzet@ens.fr>
2024-06-26 09:39:34 +0200

    Update der.ml

--- src/rewrite/der.ml
+++ src/rewrite/der.ml
@@ -21,33 +21,25 @@ open Ident
 open Zelus
 open Aux
 
-let block_of_eq s pat e =
-  { b_vars = []; b_locals = []; b_body = [eqmake (EQeq(pat, e))]; 
-    b_loc = no_location; b_write = { Deftypes.empty with dv = s };
-    b_env = Env.empty }
-
-let block_of_der s x e =
-  { b_vars = []; b_locals = []; b_body = [eqmake (EQder(x, e, None, []))]; 
-    b_loc = no_location; 
-    b_write = { Deftypes.empty with der = s }; b_env = Env.empty }
-    
-let block_spat_e_list s pat spat_e_list =
+let p_handlers id handlers =
   List.map 
-    (fun { p_cond = spat; p_body = e; p_env = env; p_zero = zero } ->
-      { p_cond = spat;
-        p_body = block_of_eq s pat e;
-        p_env = env; p_zero = zero }) spat_e_list
+    (fun { p_cond; p_body = e; p_zero; p_env; p_loc } ->
+      { p_cond; p_zero; p_env; p_loc;
+        p_body = eq id e })
+  handlers
 
-let present id e handlers eq_list =
-  let spat_b_list =
-    block_spat_e_list s (varpat x Initial.typ_float) spat_e_list in
-  (* only generate a present if [spat_b_list] is not empty *)
+let eq_and eq1 eq2 = Aux.eqmake (EQand [eq1; eq2])
+let eq_der id e = Aux.eqmake (EQder { id; e; e0_opt = None; handlers = [] })
+
+let present id e0_opt handlers eq_list =
+  let else_branch =
+    match e0_opt with
+    | None -> NoDefault | Some(e0) -> eq_init id e0 in
   match handlers with
-  | [] -> (eq_der id e) :: eq_list
+  | [] -> eq_list
   | _ ->
-     let handlers = eq_of_handlers id handlers in
-     let default_opt = Else(eq_der id e) in
-     (eqmake (EQpresent { handlers; default_opt })) :: eq_list
+     let handlers = p_handlers id handlers in
+     Aux.eq_make (EQpresent { handlers; default_opt }) :: eq_list
 
 let der_present id e e0_opt handlers =
   let eq_list =
@@ -58,7 +50,7 @@ let equation funs acc eq =
   let { eq_desc }, acc = funs.equation acc eq in
   match eq_desc with
     | EQder { id; e; e_opt; handlers } ->
-        der_present id e e0_opt handlers
+        eq_and (eq_der x e) (present id e0_opt handlers)
     | _ -> raise Mapfold.Fallback
 
 let set_index funs acc n =
