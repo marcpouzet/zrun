0fbbf70f7206ddeb96d68342071c1d480f61a895
Marc Pouzet <Marc.Pouzet@ens.fr>
2024-06-30 17:05:15 +0200

    Add new rewriting steps.

--- src/compiler/main/rewrite.ml
+++ src/compiler/main/rewrite.ml
@@ -114,9 +114,15 @@ let main ff modname filename source_name otc n_steps =
   let p = transform_and_compare
             "Compilation of initialization and resets. See below:"
             Reset.program genv0 p in
-  let _ = transform_and_compare
+  let p = transform_and_compare
             "Un-nesting of let/in and blocks. See below:"
             Letin.program genv0 p in
+  let p = transform_and_compare
+            "Complete equations with [der x = 0.0]. See below:"
+            Complete.program genv0 p in
+  let _ = transform_and_compare
+            "Static scheduling. See below:"
+            Schedule.program genv0 p in 
   ()
       (*
       let impl_list =
