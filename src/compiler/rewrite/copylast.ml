cf1fe77c48d05d7bd8f17de1e379ec6be00141b8
Marc Pouzet <Marc.Pouzet@ens.fr>
2024-07-09 10:39:44 +0200

    Update copylast.ml

--- src/compiler/rewrite/copylast.ml
+++ src/compiler/rewrite/copylast.ml
@@ -31,16 +31,17 @@
 
   fun x returns (o, y) ... last* m ... and m = y
 
-  local x, y do init x = v0 and init y = v1 and y = last x + 1 and x = last y + 1
+  local x, y, z init e
+  do init x = v0 and init y = v1 and y = last x + 1 and x = last y + 1 and ... z ...
 
   is rewritten:
 
-  local x, y, mx, my do
+  local x, y, mx, my, z init e do
   init mx = v0 and init my = v1
   y = last* mx + 1
   x = last* my + 1
   mx = x
-  my = y
+  my = y and ...
 
   An alternative would be:
 
@@ -69,11 +70,11 @@ let eq_copy (x, m) = Aux.id_eq m (Aux.var x)
 
 let eq_copy_names renaming_list = List.map eq_copy renaming_list
 
-let add_env l_env renaming_list =
+let update_env l_env renaming_list =
   List.fold_left 
     (fun acc (x, m) -> Env.add m Misc.no_info acc) l_env renaming_list
 
-let add_vardec_list b_vars renaming_list =
+let update_vardec_list b_vars renaming_list =
   List.fold_left
     (fun acc (_, m) -> Aux.vardec m true None None :: acc) 
     b_vars renaming_list
@@ -88,30 +89,12 @@ let remove l_env acc =
        with Not_found -> renaming_list, acc)
     l_env ([], acc)
 
-    (*
-      (* replace occurrences of [last x] by [lx] *)
-let rec expression funs acc ({ e_desc } as e) =
-  let e_desc, acc = match e_desc with
-    | Elast { copy; id } ->
-       if copy then
-         try (* if [id] is already in [acc] an associated to [m] *)
-           (* replace [last id] by [last*m] *)
-           let m = Env.find id acc in
-           Elast { copy = false; id = m }, acc
-         with Not_found ->
-           let m = fresh "m" in
-           Elast { copy = false; id = m }, Env.add id m acc
-       else e_desc, acc
-    | _ -> raise Mapfold.Fallback in
-  { e with e_desc }, acc
-     *)
+(* introduce a fresh copy [m] for [id] *)
 let intro acc id =
   try
-    let m = Env.find id acc in
-    m, acc
+    let m = Env.find id acc in m, acc
   with
-  | Not_found ->
-     let m = fresh "m" in m, Env.add id m acc
+  | Not_found -> let m = fresh "m" in m, Env.add id m acc
 
 (* replace [last id] by [last*m] *)
 let last_ident _ acc { copy; id } =
@@ -127,7 +110,7 @@ let rec funexp funs acc ({ f_body = { r_desc } as r; f_env } as f) =
     | Exp(e) ->
        let e, acc = Mapfold.expression funs acc e in
        let renaming_list, acc = remove f_env acc in
-       let vardec_list = add_vardec_list [] renaming_list in
+       let vardec_list = update_vardec_list [] renaming_list in
        Exp(Aux.e_local (Aux.block_make vardec_list
                           (eq_copy_names renaming_list)) e), acc
     | Returns b ->
@@ -142,7 +125,7 @@ and leq_t funs acc ({ l_eq; l_env } as leq) =
   let l_eq, acc = Mapfold.equation funs acc l_eq in
   let renaming_list, acc = remove l_env acc in
   { leq with l_eq = Aux.par (l_eq :: eq_copy_names renaming_list);
-             l_env = add_env l_env renaming_list }, acc
+             l_env = update_env l_env renaming_list }, acc
 
 and block funs acc b =
   let { b_vars; b_body; b_env; b_write } as b, acc = Mapfold.block funs acc b in
@@ -154,8 +137,8 @@ and block funs acc b =
   let b_body, acc = Mapfold.equation funs acc b_body in
   let renaming_list, acc = remove b_env acc in
   let b_body = Aux.par (b_body :: eq_copy_names renaming_list) in
-  { b with b_vars = add_vardec_list b_vars renaming_list;
-           b_env = add_env b.b_env renaming_list;
+  { b with b_vars = update_vardec_list b_vars renaming_list;
+           b_env = update_env b.b_env renaming_list;
            b_body }, acc
 									  
 let set_index funs acc n =
