738568700eef4ff4d6a4fa59d3f917d5b2a46ca6
Marc Pouzet <Marc.Pouzet@ens.fr>
2024-07-09 19:38:12 +0200

    Update

--- src/compiler/rewrite/lastinputoutput.ml
+++ src/compiler/rewrite/lastinputoutput.ml
@@ -13,17 +13,37 @@
 (* *********************************************************************)
 
 (* Add an equation [m = x] every time [last x] is used; replace [last x] *)
-(* by [lx] *)
+(* by [last*m] and rename [init x = e] in [init m = e] *)
 (* This step is necessary for static scheduling. *)
 
 (*
   Example:
 
-  local (last x), (last y), z init e
-  do init x = v0 and init y = v1 and ... y = last x + 1 and
-  ... x = last y + 1 and ... z ...
+  fun x -> last x + 1
 
   is rewritten:
+
+  fun x -> local (last m) do m = x and y = last*m + 1 in y
+
+  fun x returns (o, y) ... last y ...
+
+  is rewritten:
+
+  fun x returns (o, y) local m ... last* m ... and m = y
+
+  local x, y, z init e
+  do init x = v0 and init y = v1 and y = last x + 1 and x = last y + 1 and ... z ...
+
+  is rewritten:
+
+  local x, y, mx, my, z init e do
+  init mx = v0 and init my = v1
+  y = last* mx + 1
+  x = last* my + 1
+  mx = x
+  my = y and ...
+
+  An alternative would be:
 
   local (last x), (last y), lx, ly do
   init x = v0 and init y = v1
@@ -31,6 +51,12 @@
   lx = last* x
   y = lx + 1
   x = ly + 1
+
+  The difference in efficiency between the two is not clear; it depdends
+  on how CSE/Tomato steps are done after. The first ensures
+  that the kind of variables is unchanged. In particular, it can be applied
+  to variables that are input and outputs of functions. After this step, they
+  are not required to be state variables. 
 *)
 
 open Location
@@ -38,35 +64,23 @@ open Zelus
 open Ident
 open Aux
 
-(* renaming(x) = lx for [x] in [out_names] *)
-type acc =
-  { def_names: Misc.no_info Env.t; (* names that are defined locally in a block *)
-    renaming: Ident.t Env.t; (* for some of these names, associate [lx] to [x] *)
-  }
-  
-(* Make an equation [lx = last* x] *)
-let eq_copy x lx = Aux.id_eq lx (Aux.last_star x)
+type acc = Ident.t Env.t (* associate names to names *)
 
-(* partition a renaming in two *)
-let split l_env renaming =
-  Env.fold
-    (fun x lx (l_renaming, renaming) ->
-       if Env.mem x l_env then Env.add x lx l_renaming, renaming
-       else l_renaming, Env.add x lx renaming)
-    renaming (Env.empty, Env.empty)
-    
-let eq_copy_names l_renaming =
-  Env.fold (fun x lx acc -> eq_copy x lx :: acc) l_renaming
+(* Make an equation [m = x] *)
+let eq_copy (x, m) = Aux.id_eq m (Aux.var x)
 
-let update_env l_env l_renaming =
-  Env.fold
-    (fun x m acc -> Env.add m Misc.no_info acc) l_renaming l_env
-    
+let eq_copy_names renaming_list = List.map eq_copy renaming_list
+
+let update_env l_env renaming_list =
+  List.fold_left 
+    (fun acc (x, m) -> Env.add m Misc.no_info acc) l_env renaming_list
+
 (* an entry [local x [init e] [default v]] becomes [local m [init e] [default v] *)
 (* and an extra entry [local x] is added *)
-let update_vardec_list b_vars renaming_list =
+let update_vardec_list b_vars ming_list =
   List.fold_left
-    (fun acc (_, m) -> Aux.vardec m true None None :: acc) 
+    (fun v_list ({ var_name } as v) ->
+       try Aux.vardec x false None None;(_, m) -> Aux.vardec m true None None :: acc) 
     b_vars renaming_list
 
 (* [remove l_env acc = renaming, acc'] extract entries in [acc] from [l_env] *)
@@ -79,55 +93,51 @@ let remove l_env acc =
        with Not_found -> renaming_list, acc)
     l_env ([], acc)
 
-let intro ({ def_names; renaming } as acc) id =
+(* introduce a fresh copy [m] for [id] *)
+let intro acc id =
   try
-    let lx = Env.find id renaming in
-    lx, acc
+    let m = Env.find id acc in m, acc
   with
-  | Not_found ->
-    if S.mem id def_names then
-      let lx = fresh "lx" in lx, { acc with renaming = Env.add id lx renaming }
-    else id, acc
-                                             
-(* replace every occurrence of [last x] where [x] is a local variable *)
-(* by [lx]; an equation [lx = last*x] will be added *)
-let expression funs acc ({ e_desc } as e) =
-  match e_desc with
-  | Elast { copy; id } ->
-    if copy then
-      (* turn [last x] into [lx] *)
-      let lx, acc = intro acc id in var lx, acc
-    else e, acc
-  | _ -> raise Mapfold.Fallback
+  | Not_found -> let m = fresh "m" in m, Env.add id m acc
 
-let rec funexp funs acc ({ f_body = { r_desc } as r } as f) =
+(* replace [last id] by [last*m] *)
+let last_ident _ acc { copy; id } =
+  let m, acc = intro acc id in
+  { copy = false; id = m }, acc
+
+(* replace [init id = e] by [init m = e] *)
+let init_ident _ acc id = intro acc id
+
+let rec funexp funs acc ({ f_body = { r_desc } as r; f_env } as f) =
   let r_desc, acc =
     match r_desc with
     | Exp(e) ->
        let e, acc = Mapfold.expression funs acc e in
-       Exp(e)
+       let renaming_list, acc = remove f_env acc in
+       let vardec_list = update_vardec_list [] renaming_list in
+       Exp(Aux.e_local (Aux.block_make vardec_list
+                          (eq_copy_names renaming_list)) e), acc
     | Returns b ->
        let b, acc = block funs acc b in
        Returns b, acc in
   { f with f_body = { r with r_desc } }, acc
 
-and leq_t funs ({ def_names } as acc) ({ l_eq; l_env } as leq) =
-  let l_eq, ({ renaming } as acc) =
-    Mapfold.equation
-      funs { acc with def_names = Env.append l_env def_names } l_eq in
-  (* add equations [lx = last* x] for every [x\lx] in [renaming] *)
-  (* which is also in [l_env] *)
-  { leq with l_eq = Aux.par (l_eq :: eq_copy_names l_env renaming);
-             l_env = update_env l_env renaming },
-  { acc with def_names }
+and leq_t funs acc ({ l_eq; l_env } as leq) =
+  (* for every entry [x\m] in [acc] that appear in [l_env] *)
+  (* add an equation [m = x]; update [l_env] and [l_eq.eq_write] *)
+  (* returns the remaining [acc] *)
+  let l_eq, acc = Mapfold.equation funs acc l_eq in
+  let renaming_list, acc = remove l_env acc in
+  { leq with l_eq = Aux.par (l_eq :: eq_copy_names renaming_list);
+             l_env = update_env l_env renaming_list }, acc
 
-(* for every entry [x\m] in [acc] that appear in [b_env] *)
-(* add an equation [m = x]; update [b_env] and [b_write] *)
-(* returns the remaining [acc] *)
-and block funs acc ({ b_vars; b_body; b_env; b_write } as b) =
-  Mapfold.block funs acc b in
-  
-  let b_vars, acc =  Util.mapfold (Mapfold.vardec funs) acc b_vars in
+and block funs acc b =
+  let { b_vars; b_body; b_env; b_write } as b, acc = Mapfold.block funs acc b in
+  (* for every entry [x\m] in [acc] that appear in [b_env] *)
+  (* add an equation [m = x]; update [b_env] and [b_write] *)
+  (* returns the remaining [acc] *)
+  let b_vars, acc = 
+    Util.mapfold (Mapfold.vardec funs) acc b_vars in
   let b_body, acc = Mapfold.equation funs acc b_body in
   let renaming_list, acc = remove b_env acc in
   let b_body = Aux.par (b_body :: eq_copy_names renaming_list) in
