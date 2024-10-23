5bb924176a98e2be2de86f6179b14a93879bdc6b
Marc Pouzet <Marc.Pouzet@ens.fr>
2024-10-23 11:53:16 +0200

    -- remove useless stuff.

--- config.ml
+++ config.ml
@@ -1,4 +1,4 @@
 let version = "z0"
 let subversion = "dev"
-let stdlib = "/Users/pouzet/article/paper/lustre/hybrid/zelus_git/github/zrun.work/_build/install/default/share/zelus"
-let date = "2024-06-22-15:13"
+let stdlib = "/Users/pouzet/article/paper/lustre/hybrid/zelus_git/github/zrun.master/_build/install/default/share/zelus"
+let date = "2024-10-23-9:48"
