06d484f89999409dd263e9c4be8fa3a8d2d29ae3
Marc Pouzet <Marc.Pouzet@ens.fr>
2025-10-03 17:04:56 +0200

    Update

--- src/zrun/debug.ml
+++ src/zrun/debug.ml
@@ -49,6 +49,18 @@ let print_state comment s =
 let print_program p =
   if !verbose then Printer.program Format.err_formatter p
 
+let incr_total_number_of_iterations_in_fixpoints n =
+  if !Misc.compute_total_number_of_iterations_in_fixpoints
+  then Misc.incr_total_number_of_iterations_in_fixpoints n
+let reset_total_number_of_iterations_in_fixpoints () =
+  if !Misc.compute_total_number_of_iterations_in_fixpoints
+  then Misc.reset_total_number_of_iterations_in_fixpoints ()
+let print_total_number_of_iterations_in_fixpoints () =
+  if !Misc.compute_total_number_of_iterations_in_fixpoints then
+    Format.eprintf
+      "The total number of iterations for fixpoint computation is: %d\n"
+      !Misc.total_number_of_iterations_in_fixpoints 
+
 let print_nothing _ = ()
 
 let counter = ref 0
