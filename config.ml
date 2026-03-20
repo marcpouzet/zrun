f49e58a45bb8a3734a1891598dd2bbe2ad82c808
Marc Pouzet <Marc.Pouzet@ens.fr>
2026-03-20 16:28:50 +0100

    Update config.ml

--- config.ml
+++ config.ml
@@ -1,4 +1,4 @@
 let version = "Zrun.2025"
 let subversion = "dev"
 let stdlib = "/Users/pouzet/article/paper/lustre/hybrid/zelus_git/github/zrun.2025/_build/install/default/share/zrun"
-let date = "2026-03-17-9:30"
+let date = "2026-03-20-14:36"
